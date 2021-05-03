32c7449be9ec0b76978ca44b968d9feee5492f31
Paco Gomez <p.gomez@geekshubs.com>
2021-05-03 18:44:42 +0200

    Ejemplo HTML-CSS-JS

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function convertirEuros(){
+    var euros;
+    var dolares;
+    var yenes;
+
+    euros=document.getElementById("euros");
+    console.log(euros.value);
+    dolares=document.getElementById("dolares");
+    dolares.value=euros.value*1.21;
+    yenes=document.getElementById("yenes");
+    yenes.value=euros.value*130,74;
+    return false;
+}
